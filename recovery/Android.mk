b42d2770c82ff73136b65c83f6947b138525daa0
LuK1337 <priv.luk@gmail.com>
2018-08-12 20:51:09 +0200

    ocean: Fix build of librecovery_updater_ocean
    Change-Id: I308fca4987b049266ebaae04de8b3e0efbf836ba

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -4,7 +4,10 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := librecovery_updater_ocean
 
-LOCAL_C_INCLUDES := $(call project-path-for,recovery)
+LOCAL_C_INCLUDES := \
+    bootable/recovery \
+    bootable/recovery/edify/include \
+    bootable/recovery/otautil/include
 
 LOCAL_SRC_FILES := recovery_updater.cpp
 
