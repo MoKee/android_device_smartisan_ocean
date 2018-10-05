82951491456d9579ed036159c81748a54260cb29
Vinay Verma <vvinay@codeaurora.org>
2018-10-05 12:38:44 +0530

    ocean: Remove firmware class path for kernel firmware loading
    In ueventd support has been added to read firmware path from
    vendor. It wil parse /vendor/firmware_mnt/image information
    and add in look up path/search path for loading firmware.
    Change uid of /vendor/firmware_mnt back to system as kernel
    firmware loading is removed. Previously root permissions were
    required to support kernel firmware loading.
    
    Change-Id: I560ed75ee07626094acbc879b615190cf3a9faa3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,7 +44,6 @@ TARGET_USES_64_BIT_BINDER := true
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 androidboot.configfs=true androidboot.usbcontroller=a600000.dwc3 swiotlb=1 printk.devkmsg=on
-BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET := 0x01000000
