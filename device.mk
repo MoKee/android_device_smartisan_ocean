b947e4ef64f73e85a9f380bc6502915f8f3d6e59
GuaiYiHu <guaiyihu@foxmail.com>
2018-12-11 23:18:56 +0800

    ocean: proprietary: Update Radio blobs
    * Update ims library symlinks
    * Update props for new radio blobs
    * Uprev radio and ims version
    
    Change-Id: Ib06c9682907917c875884b2f1963002e9622663c

--- device.mk
+++ device.mk
@@ -240,6 +240,9 @@ PRODUCT_PACKAGES += \
 
 # Radio
 PRODUCT_PACKAGES += \
+    android.hardware.radio@1.2 \
+    android.hardware.radio.config@1.0 \
+    android.hardware.secure_element@1.0 \
     ims-ext-common \
     libjson \
     librmnetctl \
