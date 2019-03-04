0f609161eb19cea5fe8fe6a15660725e288ace7c
XiNGRZ <chenxingyu92@gmail.com>
2019-03-04 11:00:29 +0800

    ocean: Import media configs from CAF tag LA.UM.6.8.r1-06700-SDM710.0
    Change-Id: I5324f6106860e4e6362d281712d2de15fdb19376

--- device.mk
+++ device.mk
@@ -207,7 +207,8 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/media/media_profiles_vendor.xml:system/etc/media_profiles_vendor.xml
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/media,$(TARGET_COPY_OUT_VENDOR)/etc) \
+    $(LOCAL_PATH)/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
