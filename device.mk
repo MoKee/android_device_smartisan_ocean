a10eee6318d1892d1fa9c51011269cfe0302fc17
XiNGRZ <chenxingyu92@gmail.com>
2019-05-20 19:05:11 +0800

    ocean: Uprev audio effect HAL
    Change-Id: Ie6963952f1b35c0bb39f502bd52dfda03c09a5b3

--- device.mk
+++ device.mk
@@ -77,7 +77,7 @@ PRODUCT_CHARACTERISTICS := nosdcard
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@4.0-impl \
     android.hardware.soundtrigger@2.0-impl \
     audio.a2dp.default \
     audio.r_submix.default \
