4a6515f6c8f8d26d1d1a5d60d14b3cf7e5fc038d
XiNGRZ <chenxingyu92@gmail.com>
2019-05-29 20:31:06 +0800

    ocean: Uprev SoundTrigger 2.1
    Change-Id: I92517a854a9c9f616da1004625ca6d0c4917123a

--- device.mk
+++ device.mk
@@ -79,7 +79,7 @@ PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@4.0-impl \
-    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
