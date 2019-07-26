a0aa8872fa52a84fcdfc77c529215cf1594aa301
XiNGRZ <chenxingyu92@gmail.com>
2019-07-26 14:25:00 +0800

    ocean: Import dalvik heap properties from stock
    dalvik.vm.heapgrowthlimit in phone-xhdpi-2048-dalvik-heap.mk is 192m,
    while stock value is 256m.
    
    Change-Id: I4031db24e135859cc3473fcbdf317edfd9b638b8

--- device.mk
+++ device.mk
@@ -70,7 +70,13 @@ TARGET_SCREEN_HEIGHT := 2160
 TARGET_SCREEN_WIDTH := 1080
 TARGET_BOOTANIMATION_INTRO := $(LOCAL_PATH)/bootanimation
 
-$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapmaxfree=8m
 
 # Haters gonna hate..
 PRODUCT_CHARACTERISTICS := nosdcard
