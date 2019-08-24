16ba5a0552c823dbeb8f481d7887f2f89351e750
XiNGRZ <chenxingyu92@gmail.com>
2019-08-24 17:15:50 +0800

    ocean: keyhandler: Migrate to new screenshot method
    Change-Id: Ie073160b2fa4144e84ac3cf8cdbc9ae6f97ca096

--- configpanel/src/org/mokee/settings/device/KeyHandler.java
+++ configpanel/src/org/mokee/settings/device/KeyHandler.java
@@ -21,6 +21,7 @@ import android.hardware.input.InputManager;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.PowerManager;
+import android.os.RemoteException;
 import android.os.SystemClock;
 import android.util.Log;
 import android.view.InputDevice;
@@ -28,9 +29,9 @@ import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
 import android.view.ViewConfiguration;
 import android.view.WindowManager;
+import android.view.WindowManagerGlobal;
 
 import com.android.internal.os.DeviceKeyHandler;
-import com.android.internal.util.ScreenshotHelper;
 
 import mokee.providers.MKSettings;
 
@@ -52,7 +53,6 @@ public class KeyHandler implements DeviceKeyHandler {
 
     private Context context;
     private PowerManager pm;
-    private ScreenshotHelper screenshotHelper;
 
     private boolean shortcutsKeyPressed = false;
     private boolean powerKeyPressed = false;
@@ -75,7 +75,6 @@ public class KeyHandler implements DeviceKeyHandler {
     public KeyHandler(Context context) {
         this.context = context;
         this.pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
-        this.screenshotHelper = new ScreenshotHelper(context);
     }
 
     public KeyEvent handleKeyEvent(KeyEvent event) {
@@ -174,7 +173,11 @@ public class KeyHandler implements DeviceKeyHandler {
                 ? WindowManager.TAKE_SCREENSHOT_SELECTED_REGION
                 : WindowManager.TAKE_SCREENSHOT_FULLSCREEN;
 
-        screenshotHelper.takeScreenshot(type, true, true, handler);
+        try {
+            WindowManagerGlobal.getWindowManagerService().takeScreenshot(type);
+        } catch (RemoteException e) {
+            Log.e(TAG, "Error while trying to takeScreenshot.", e);
+        }
     }
 
     private class KeyInfo {
