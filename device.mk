50fbed78c1900d504ce505df46914d8e52cf24f1
XiNGRZ <chenxingyu92@gmail.com>
2019-11-14 15:46:23 +0800

    ocean: Drop Snap
    Change-Id: I2d7ea65030864c2867e94214e4f34ea480d370c3

--- device.mk
+++ device.mk
@@ -124,9 +124,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service
 
-PRODUCT_PACKAGES += \
-    Snap
-
 # Config panel
 PRODUCT_PACKAGES += \
     ConfigPanel
