27c58bf5f1c206eab8aa81073229a33bf220f753
XiNGRZ <chenxingyu92@gmail.com>
2020-02-13 09:53:06 +0800

    ocean: Use charger from stock
    Change-Id: Ib1117d65d76bd16436395f266184f9b46325b162

--- device.mk
+++ device.mk
@@ -125,6 +125,10 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service
 
+# Charger
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/charger,root/res/images/charger)
+
 # Config panel
 PRODUCT_PACKAGES += \
     ConfigPanel
